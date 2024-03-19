4cb694b000d87fa6aa102bd74e49c359eab69733
mohamadrostami <mb.rostami.h@gmail.com>
2024-03-19 11:18:34 +0100

    add 64 bit version

--- options.go
+++ options.go
@@ -2,6 +2,7 @@ package consistenthash
 
 type options struct {
 	hashFunc          HashFunc
+	hashFunc64        HashFunc64
 	initialRingSize   int
 	defaultReplicas   uint
 	blockPartitioning int
@@ -11,15 +12,24 @@ type options struct {
 
 type Option func(*options)
 
+// WithDefaultReplicas default number of replicas to add for each key
 func WithDefaultReplicas(replicas uint) Option {
 	return func(o *options) {
 		o.defaultReplicas = replicas
 	}
 }
 
+// WithHashFunc hash function for 32bit CH
 func WithHashFunc(hashFunc HashFunc) Option {
 	return func(o *options) {
 		o.hashFunc = hashFunc
+	}
+}
+
+// WithHashFunc64 hash function for 64bit CH
+func WithHashFunc64(hashFunc HashFunc64) Option {
+	return func(o *options) {
+		o.hashFunc64 = hashFunc
 	}
 }
 
@@ -30,7 +40,7 @@ func WithReadLockFree(readLockFree bool) Option {
 	}
 }
 
-// WithBlockPartitioning uses block partitioning, divides total number of keys to divisionBy to get the number of blocks
+// WithBlockPartitioning uses block partitioning, divides total number of keys to the given number to get number of blocks
 func WithBlockPartitioning(divisionBy int) Option {
 	return func(o *options) {
 		o.blockPartitioning = divisionBy
@@ -38,6 +48,7 @@ func WithBlockPartitioning(divisionBy int) Option {
 }
 
 // WithMetrics enables collecting metrics for block partitioning
+// using metrics is not thread safe, and needs to be used only for test and debugging
 func WithMetrics() Option {
 	return func(o *options) {
 		o.metrics = true

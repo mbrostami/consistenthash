3ee2fb7f64a9d1af27b661ae6acd375d34b67b49
mohamadrostami <mrostami@harlemnext.com>
2024-04-04 02:02:32 +0200

    Add comments for exported type Option in options.go

--- options.go
+++ options.go
@@ -1,28 +1,30 @@
 package consistenthash
 
+// options struct holds the configurable options for consistent hash
 type options struct {
 	hashFunc          HashFunc
 	defaultReplicas   uint
 	blockPartitioning int
 }
 
+// Option is a function type used to apply configurable options to consistent hash
 type Option func(*options)
 
-// WithDefaultReplicas default number of replicas to add for each key
+// WithDefaultReplicas sets the default number of replicas to add for each key
 func WithDefaultReplicas(replicas uint) Option {
 	return func(o *options) {
 		o.defaultReplicas = replicas
 	}
 }
 
-// WithHashFunc hash function for 32bit CH
+// WithHashFunc sets the hash function for 32bit CH
 func WithHashFunc(hashFunc HashFunc) Option {
 	return func(o *options) {
 		o.hashFunc = hashFunc
 	}
 }
 
-// WithBlockPartitioning uses block partitioning, divides total number of keys to the given number to get number of blocks
+// WithBlockPartitioning applies block partitioning by dividing total number of keys
 func WithBlockPartitioning(divisionBy int) Option {
 	return func(o *options) {
 		o.blockPartitioning = divisionBy
